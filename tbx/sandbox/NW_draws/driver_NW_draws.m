3c70a73c04e2fa7c1c2e342c215f660f32071b38
kobercs13 <csaba.kober@ogresearch.com>
2024-06-04 15:14:57 +0200

    adding folder generating NW draws

---
+++ tbx/sandbox/NW_draws/driver_NW_draws.m
@@ -0,0 +1,32 @@
+
+%% Loading data
+Y = readmatrix("Y.csv");
+X = readmatrix("X.csv");
+
+% load("data.mat")
+%% Setting up opt structure
+fileName = "NW_opts.json"; % filename in JSON extension.
+str      = fileread(fileName); % dedicated for reading files as text.
+opt      = jsondecode(str);
+
+% opt.priorsexogenous = 0;
+% 
+% % hyperparameters
+% opt.user_ar = 1;
+% opt.lambda1 = 0.1;
+% opt.lambda3 = 1;
+% opt.lambda4 = 100;
+% 
+% %prior type
+% opt.prior = 21; %NW, S0 as univariate AR with 
+% 
+% %data matrices and sizes
+% opt.p          = 4; 
+% opt.const      = true;
+% 
+% %Settings for sampling
+% opt.It = 2000;
+% opt.Bu = 1000;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% getting the draws
+[beta_gibbs, sigma_gibbs] = get_NW_draws(Y, X, opt);
