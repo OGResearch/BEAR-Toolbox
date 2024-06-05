35b8b94b347ab2e87dae9dca0336a67ccf42959f
kobercs13 <csaba.kober@ogresearch.com>
2024-06-05 10:52:20 +0200

    adding separtae estimation for NW_FAVAR

---
+++ tbx/sandbox/NW_FAVAR_draws/driver_NW_FAVAR_draws.m
@@ -0,0 +1,26 @@
+
+%% Loading data
+data_endo = readmatrix("data_endo.csv");
+data_exo = readmatrix("data_exo.csv");
+
+load("FAVAR.mat")
+%% Setting up opt structure
+fileName = "NW_FAVAR_opts.json"; % filename in JSON extension.
+str      = fileread(fileName); % dedicated for reading files as text.
+tmp     = jsondecode(str);
+
+opts = BEARsettings(tmp.VARtype, 'ExcelFile', fullfile(bearroot(), tmp.Datafolder,tmp.Datafile));
+fn = fieldnames(tmp);
+for ii = 1:numel(fieldnames(tmp))
+    try
+    opts.(fn{ii}) = tmp.(fn{ii});
+    end
+end
+
+fileName = "NW_FAVAR_favar.json"; % filename in JSON extension.
+str      = fileread(fileName); % dedicated for reading files as text.
+favar    = jsondecode(str);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% getting the draws
+[beta_gibbs, sigma_gibbs, favar] = get_NW_FAVAR_draws(opts,favar);
