2435df2da6903ad3a8de9beaee82003bc3bba555
kobercs13 <csaba.kober@ogresearch.com>
2024-06-12 06:36:20 +0200

    initialize cswf (dummy_to_YLX)

--- tbx/sandbox/+test/dummies.m
+++ tbx/sandbox/+test/dummies.m
@@ -22,6 +22,19 @@ meta = model.ReducedForm.Meta( ...
 
 prior = prior.NormalWishart();
 
+data_endo = nan(numel(dataSpan), 0);
+for i = 1:numel(meta.EndogenousItems)
+    item = meta.EndogenousItems{i};
+    data_endo = [data_endo, item.getData(hist, dataSpan, variant=1)];
+end
+
+constant_flag = isa(meta.ExogenousItems{end},'item.Constant');
+data_exo = nan(numel(dataSpan), 0);
+for i = 1:numel(meta.ExogenousItems) - double(constant_flag)
+    item = meta.ExogenousItems{i};
+    data_exo = [data_exo, item.getData(hist, dataSpan, variant=1)];
+end
+
 v = model.ReducedForm( ...
     meta=meta, ...
     estimator=prior, ...
@@ -30,9 +43,46 @@ v = model.ReducedForm( ...
 
 
 opt = dummies.populateLegacyOptions(v.Dummies);
+opt.excelFile = "C://Git//BEAR-toolbox-6//tbx//replications//data_.xlsx"; %needed for location of lr priors
+opt.priorsexogenous  = false; %set true if want to use individual priors for exo 
+
+
 YLX = v.getDataYLX(hist, dataSpan);
 
-% [Ystar, LXstar] = csaba_wonderful_function(opt, YLX)
+[Ystar, LXstar] = dummy_to_YLX(opt,meta.Order,constant_flag, data_endo,data_exo, YLX);
 
 disp(opt)
 
+function [Ystar, LXstar] = dummy_to_YLX(opt,order,constant, data_endo,data_exo, YLX)
+
+Y = YLX{1};
+X = [YLX{2} YLX{3}];
+
+n = size(data_endo,2);
+T = size(data_endo,1);
+m = size(data_exo,2) + constant;
+
+H = [];
+if opt.lrp==1
+    H=bear.loadH(struct('excelFile', opt.excelFile));
+end
+
+
+[Ystar, ~, Xstar, Tstar, ~, ~, ~, ~] = ...
+    bear.gendummy(data_endo,data_exo,Y,X,n,m,order,T,constant,opt.lambda6,opt.lambda7,opt.lambda8,opt.scoeff,opt.iobs,opt.lrp,H);
+
+if opt.prior == 51
+
+    for ii=1:n
+        for jj=1:m
+            priorexo(ii,jj) = opt.priorsexogenous;
+        end
+    end
+    
+    [arvar] = bear.arloop(Y,constant,order,n);
+    ar = ones(n,1)*opt.ar;
+    [Ystar,LXstar,Tstar]=bear.doprior(Ystar,Xstar,n,m,order,Tstar,ar,arvar,opt.lambda1,opt.lambda3,opt.lambda4,priorexo);
+
+
+end
+end
