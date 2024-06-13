c9ab73d9d5cf717d4b2ad4d6d19ac67d39450efb
kobercs13 <csaba.kober@ogresearch.com>
2024-06-13 10:06:51 +0200

    spit gendummy function

--- tbx/sandbox/+test/dummies.m
+++ tbx/sandbox/+test/dummies.m
@@ -1,5 +1,3 @@
-
-
 close all
 clear
 clear classes
@@ -22,18 +20,6 @@ meta = model.ReducedForm.Meta( ...
 
 prior = prior.NormalWishart();
 
-data_endo = nan(numel(dataSpan), 0);
-for i = 1:numel(meta.EndogenousItems)
-    item = meta.EndogenousItems{i};
-    data_endo = [data_endo, item.getData(hist, dataSpan, variant=1)];
-end
-
-constant_flag = isa(meta.ExogenousItems{end},'item.Constant');
-data_exo = nan(numel(dataSpan), 0);
-for i = 1:numel(meta.ExogenousItems) - double(constant_flag)
-    item = meta.ExogenousItems{i};
-    data_exo = [data_exo, item.getData(hist, dataSpan, variant=1)];
-end
 
 v = model.ReducedForm( ...
     meta=meta, ...
@@ -43,46 +29,74 @@ v = model.ReducedForm( ...
 
 
 opt = dummies.populateLegacyOptions(v.Dummies);
-opt.excelFile = "C://Git//BEAR-toolbox-6//tbx//replications//data_.xlsx"; %needed for location of lr priors
-opt.priorsexogenous  = false; %set true if want to use individual priors for exo 
-
-
 YLX = v.getDataYLX(hist, dataSpan);
 
-[Ystar, LXstar] = dummy_to_YLX(opt,meta.Order,constant_flag, data_endo,data_exo, YLX);
+%%%%obsolete block delete once LHS vars are created loaded elsewhere
+opt.excelFile = "C://Git//BEAR-toolbox-6//tbx//replications//data_.xlsx"; %needed for location of lr priors
+opt.priorsexogenous  = false; %set true if want to use individual priors for exo 
+H = bear.loadH(struct('excelFile', opt.excelFile));
+
+ds = dataSpan(1:meta.Order);
+init_endo = nan(numel(ds), 0);
+n = numel(meta.EndogenousItems);
+for i = 1:numel(meta.EndogenousItems)
+    item = meta.EndogenousItems{i};
+    init_endo = [init_endo, item.getData(hist, ds, variant=1)];
+end
+
+init_exo = nan(numel(ds), 0);
+m = numel(meta.ExogenousItems);
+for i = 1:m
+    item = meta.ExogenousItems{i};
+    init_exo = [init_exo, item.getData(hist, ds, variant=1)];
+end
+
+for ii=1:n
+    for jj=1:m
+        priorexo(ii,jj) = opt.priorsexogenous;
+        lambda4(ii,jj) = opt.lambda4;
+    end
+end
+opt.lambda4 = lambda4;    
+[arvar] = bear.arloop(YLX{1},isa(meta.ExogenousItems(end),"item.Constant"),meta.Order,n);
+ar = ones(n,1)*opt.ar;
+
+%%%%obsolete block end
+
+[Ystar, LXstar] = dummy_to_YLX(YLX, init_endo, init_exo,meta.Order, opt, H , ar, arvar, priorexo);
 
 disp(opt)
 
-function [Ystar, LXstar] = dummy_to_YLX(opt,order,constant, data_endo,data_exo, YLX)
+function [Ystar, LXstar] = dummy_to_YLX(YLX, init_endo, init_exo, order, opt, H, ar, arvar, priorexo)
 
-Y = YLX{1};
-X = [YLX{2} YLX{3}];
+Ystar = YLX{1};
+LXstar = [YLX{2} YLX{3}];
 
-n = size(data_endo,2);
-T = size(data_endo,1);
-m = size(data_exo,2) + constant;
+n = size(init_endo,2);
+m = size(init_exo,2);
 
-H = [];
-if opt.lrp==1
-    H=bear.loadH(struct('excelFile', opt.excelFile));
+if opt.scoeff
+    [Ys, LXs ]  = test.get_scoeff_dummy(init_endo, n,m,order,opt.lambda6);
+    Ystar = [Ystar;Ys];
+    LXstar = [LXstar;LXs];
 end
 
+if opt.iobs
+    [Yo, LXo ]  = test.get_iobs_dummy(init_endo,init_exo,order,opt.lambda7);
+    Ystar = [Ystar;Yo];
+    LXstar = [LXstar;LXo];
+end
 
-[Ystar, ~, Xstar, Tstar, ~, ~, ~, ~] = ...
-    bear.gendummy(data_endo,data_exo,Y,X,n,m,order,T,constant,opt.lambda6,opt.lambda7,opt.lambda8,opt.scoeff,opt.iobs,opt.lrp,H);
+if opt.lrp
+    [Yl, LXl ]  = test.get_lrp_dummy(init_endo,H, n,m,order,opt.lambda8);
+    Ystar = [Ystar;Yl];
+    LXstar = [LXstar;LXl];
+end
 
 if opt.prior == 51
-
-    for ii=1:n
-        for jj=1:m
-            priorexo(ii,jj) = opt.priorsexogenous;
-        end
-    end
-    
-    [arvar] = bear.arloop(Y,constant,order,n);
-    ar = ones(n,1)*opt.ar;
-    [Ystar,LXstar,Tstar]=bear.doprior(Ystar,Xstar,n,m,order,Tstar,ar,arvar,opt.lambda1,opt.lambda3,opt.lambda4,priorexo);
-
-
+    [Ym, LXm ]  = test.get_min_dummy(n,m,order,ar,arvar,opt.lambda1,opt.lambda3,opt.lambda4,priorexo);
+    Ystar = [Ystar;Ym];
+    LXstar = [LXstar;LXm];
 end
+
 end
