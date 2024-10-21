47cd0ba7709820b491a45599035dcb23787d5cc0
Ljubica Trajcev <ljubica.trajcev@ogresearch.com>
2024-10-21 09:19:14 +0200

    panel random effect with hierarchical prior

--- tbx/sandbox/PANEL/lj_panel_rand_eff_hier_drawer.m
+++ tbx/sandbox/PANEL/lj_panel_rand_eff_hier_drawer.m
@@ -1,49 +1,97 @@
-function [A, C, Sigma] = lj_panel_rand_eff_hier_drawer(smpl,numCountries,numEndog,numLags,numExog)
-    % input 
-    % smpl - one sample (gibbs sampling) that contains:
-    % smpl.beta - one sample of beta gibbs
-    % smpl.sigma - one sample of sigma gibbs
+function [outUnconditionalDrawer, outIdentifierDrawer] = lj_panel_rand_eff_hier_drawer(this, meta)
 
-    % output
-    % A - transformed matrix of parameters in front of transition variables
-    % C - tranformed matrix of parameters in front of exogenous and constant
-    % Sigma - transformed matrix of variance covariance of shocks
-    % Y = (L)Y*A + X*C + eps
+    numCountries = meta.numCountries;
+    numEndog     = meta.numEndog;
+    numLags      = meta.numLags;
+    numExog      = meta.numExog;
     
-    beta = smpl.beta;
-    sigma = smpl.sigma;
+    %IRF periods
+    IRFperiods = meta.IRFperiods;
+
+    EstimationSpan = this.EstimationSpan;
     
-    % initialization
-    A = [];
-    C = [];
+    function [As, Cs, Sigma] = identificationDrawer(sampleStruct)
 
-    Sigma = [];
+        % input 
+        % smpl - one sample (gibbs sampling) that contains:
+        % smpl.beta - one sample of beta gibbs
+        % smpl.sigma - one sample of sigma gibbs
 
-    for ii = 1:numCountries
+        % output
+        % A - transformed matrix of parameters in front of transition variables
+        % C - tranformed matrix of parameters in front of exogenous and constant
+        % Sigma - transformed matrix of variance covariance of shocks
+        % Y = (L)Y*A + X*C + eps
 
-      beta_temp = reshape(...
-                beta(:,ii),...
-                numEndog*numLags+numExog,...
-                numEndog...
-                );
+        smpl = sampleStruct;
+        beta = smpl.beta;
+        sigma = smpl.sigma;
+        
+        % initialization
+        A = [];
+        C = [];
 
-      sigma_temp = reshape(...
-                sigma(:,ii),...
-                numEndog,...
-                numEndog...
-                );
+        Sigma = [];
 
-      % Pack in blocks
-      a_temp = beta_temp(1:numEndog*numLags,:);
+        % initialize the output
+        As = cell(IRFperiods,1);
+        Cs = cell(IRFperiods,1);
 
-      c_temp = beta_temp(numEndog*numLags+1:end,:);
+        for ii = 1:numCountries
 
-      A = blkdiag(A, a_temp);
+            beta_temp = reshape(...
+                      beta(:,ii),...
+                      numEndog*numLags+numExog,...
+                      numEndog...
+                      );
 
-      C = [C, c_temp];
-      
-      Sigma = blkdiag(Sigma,sigma_temp);
+            sigma_temp = reshape(...
+                      sigma(:,ii),...
+                      numEndog,...
+                      numEndog...
+                      );
+
+            % Pack in blocks
+            a_temp = beta_temp(1:numEndog*numLags,:);
+
+            c_temp = beta_temp(numEndog*numLags+1:end,:);
+
+            A = blkdiag(A, a_temp);
+
+            C = [C, c_temp];
+            
+            Sigma = blkdiag(Sigma,sigma_temp);
+
+        end
+
+        % pack the output
+        for tt = 1:IRFperiods
+
+            As{tt} = A;
+            Cs{tt} = C;
+
+        end
+    end
+
+    function [As, Cs, Sigmas] = unconditionalDrawer(sampleStruct, forecastStart,forecastHorizon)
+
+        % call the identificationDrawer as for the time invariant models, results are almost the same
+        [As, Cs, Sigma] = identificationDrawer(sampleStruct, forecastHorizon);
+
+        Sigmas  = cell(forecastHorizon,1);
+
+        % pack the output
+        for tt = 1:forecastHorizon
+
+            Sigmas{tt} = Sigma;
+
+        end
 
     end
+
+    % return function calls
+    outIdentifierDrawer = @identificationDrawer;
+
+    outUnconditionalDrawer = @unconditionalDrawer;
     
 end
--- tbx/sandbox/PANEL/lj_panel_rand_eff_hier_smpl.m
+++ tbx/sandbox/PANEL/lj_panel_rand_eff_hier_smpl.m
@@ -1,7 +1,27 @@
-function outSampler = lj_panel_rand_eff_hier_smpl(data_endo,data_exo,const,lags,lambda2,lambda3,lambda4,s0,v0, Bu)
+function outSampler = lj_panel_rand_eff_hier_smpl(this, meta, longY, longX, longZ)
+
+    % input
+    % meta - model specific meta parameters
+    % hyper - model specific hyper parameters
+    % longY - matrix with endogenous variables (whole sample)
+    % longX - matrix with exogenous variables (whole sample)
+    % longZ - matrix with reducibles (whole sample, used for factor models)
+
+    % output
+    % outSampler - sampler function
+    
+    const = meta.flagConst;
+    lags  = meta.numLags;
+
+    lambda2 = this.Settings.lambda2;
+    lambda3 = this.Settings.lambda3;
+    lambda4 = this.Settings.lambda4;
+    s0 = this.Settings.s0;
+    v0 = this.Settings.v0;
+    Bu = this.Settings.Bu;
 
     % compute preliminary elements
-    [Xi, ~, ~, Yi, ~, y, numCountries, numEndog, numExog, numLags, T, ~, ~, ~]=bear.panel4prelim(data_endo,data_exo,const,lags);
+    [Xi, ~, ~, Yi, ~, y, numCountries, numEndog, numExog, numLags, T, ~, ~, ~]=bear.panel4prelim(longY,longX,const,lags);
 
     % determine k, the number of parameters to estimate in each equation
     k=numEndog*numLags+numExog;
@@ -22,18 +42,11 @@ function outSampler = lj_panel_rand_eff_hier_smpl(data_endo,data_exo,const,lags,
     % compute the inverse of omegab
     invomegab=diag(1./diag(omegab));
 
-    % initiate the record matrices
-    % beta_gibbs=zeros(q,It-Bu,N);
-    % sigma_gibbs=zeros(n^2,It-Bu,N);
-    % beta_mean=zeros(q,It-Bu);
-    % sigma_mean=zeros(n^2,It-Bu);
-    % lambda_posterior=zeros((It-Bu),1);
-
     % step 1: compute initial values
     % initial value for beta (use OLS values)
     for ii=1:numCountries
 
-      beta_init(:,ii)=bear.vec((Xi(:,:,ii)'*Xi(:,:,ii))\(Xi(:,:,ii)'*Yi(:,:,ii)));
+        beta_init(:,ii)=bear.vec((Xi(:,:,ii)'*Xi(:,:,ii))\(Xi(:,:,ii)'*Yi(:,:,ii)));
 
     end
 
@@ -48,9 +61,9 @@ function outSampler = lj_panel_rand_eff_hier_smpl(data_endo,data_exo,const,lags,
     % initial value for sigma (use OLS values)
     for ii=1:numCountries
 
-      eps=Yi(:,:,ii)-Xi(:,:,ii)*reshape(beta_init(:,ii),k,numEndog);
+        eps=Yi(:,:,ii)-Xi(:,:,ii)*reshape(beta_init(:,ii),k,numEndog);
 
-      sigma(:,:,ii)=(1/(T-k-1))*eps'*eps;
+        sigma(:,:,ii)=(1/(T-k-1))*eps'*eps;
 
     end
 
@@ -59,95 +72,93 @@ function outSampler = lj_panel_rand_eff_hier_smpl(data_endo,data_exo,const,lags,
     % sampler will be a function
     function smpl = sampler()
 
-      % step 2: obtain b
-      % first compute betam, the mean value of the betas over all units
-      betam=(1/numCountries)*sum(beta,2);
+        % step 2: obtain b
+        % first compute betam, the mean value of the betas over all units
+        betam=(1/numCountries)*sum(beta,2);
 
-      % draw b from a multivariate normal N(betam,(1/numCountries)*sigmab))
-      b=betam+chol(bear.nspd((1/numCountries)*sigmab),'lower')*mvnrnd(zeros(q,1),eye(q))';
+        % draw b from a multivariate normal N(betam,(1/numCountries)*sigmab))
+        b=betam+chol(bear.nspd((1/numCountries)*sigmab),'lower')*mvnrnd(zeros(q,1),eye(q))';
 
-      % step 3: obtain sigmab
-      % compute first vbar
-      for ii=1:numCountries
+        % step 3: obtain sigmab
+        % compute first vbar
+        for ii=1:numCountries
 
-          temp(1,ii)=(beta(:,ii)-b)'*invomegab*(beta(:,ii)-b);
+            temp(1,ii)=(beta(:,ii)-b)'*invomegab*(beta(:,ii)-b);
 
-      end
+        end
 
-      vbar=v0+sum(temp,2);
+        vbar=v0+sum(temp,2);
 
-      % compute lambda1
-      lambda1=bear.igrandn(sbar/2,vbar/2);
+        % compute lambda1
+        lambda1=bear.igrandn(sbar/2,vbar/2);
 
-      % recover sigmab
-      sigmab=lambda1*omegab;
+        % recover sigmab
+        sigmab=lambda1*omegab;
 
-      % step 4: draw the series of betas
-      % first obtain the inverse of sigmab
-      invsigmab=diag(1./diag(sigmab));
-      
-      % then loop over units
-      for ii=1:numCountries
-
-        % take the choleski factor of sigma of unit ii, inverse it, and obtain from it the inverse of the original sigma
-        C=bear.trns(chol(bear.nspd(sigma(:,:,ii)),'Lower'));
-
-        invC=C\speye(numEndog);
-
-        invsigma=invC*invC';
-
-        % obtain omegabar
-        invomegabar=kron(invsigma,Xi(:,:,ii)'*Xi(:,:,ii))+invsigmab;
-
-        % invert
-        C=bear.trns(chol(bear.nspd(invomegabar),'Lower'));
-
-        invC=C\speye(q);
+        % step 4: draw the series of betas
+        % first obtain the inverse of sigmab
+        invsigmab=diag(1./diag(sigmab));
         
-        omegabar=invC*invC';
+        % then loop over units
+        for ii=1:numCountries
 
-        % obtain betabar
-        betabar=omegabar*(kron(invsigma,Xi(:,:,ii)')*bear.vec(Yi(:,:,ii))+invsigmab*b);
+            % take the choleski factor of sigma of unit ii, inverse it, and obtain from it the inverse of the original sigma
+            C=bear.trns(chol(bear.nspd(sigma(:,:,ii)),'Lower'));
 
-        % draw beta
-        beta(:,ii)=betabar+chol(bear.nspd(omegabar),'lower')*mvnrnd(zeros(q,1),eye(q))';
+            invC=C\speye(numEndog);
 
-        beta_gibbs(:,ii)=beta(:,ii);
+            invsigma=invC*invC';
 
-      end
+            % obtain omegabar
+            invomegabar=kron(invsigma,Xi(:,:,ii)'*Xi(:,:,ii))+invsigmab;
 
-      % step 5: draw the series of sigmas
-      % loop over units
-      for ii=1:numCountries
+            % invert
+            C=bear.trns(chol(bear.nspd(invomegabar),'Lower'));
 
-        % compute Stilde
-        Stilde=(Yi(:,:,ii)-Xi(:,:,ii)*reshape(beta(:,ii),k,numEndog))'*(Yi(:,:,ii)-Xi(:,:,ii)*reshape(beta(:,ii),k,numEndog));
+            invC=C\speye(q);
+            
+            omegabar=invC*invC';
 
-        % draw sigma
-        sigma(:,:,ii)=bear.iwdraw(Stilde,T);
+            % obtain betabar
+            betabar=omegabar*(kron(invsigma,Xi(:,:,ii)')*bear.vec(Yi(:,:,ii))+invsigmab*b);
 
-        sigma_gibbs(:,ii)=bear.vec(sigma(:,:,ii));
+            % draw beta
+            beta(:,ii)=betabar+chol(bear.nspd(omegabar),'lower')*mvnrnd(zeros(q,1),eye(q))';
 
-      end
+            beta_gibbs(:,ii)=beta(:,ii);
 
-      beta_mean=b;
+        end
 
-      sigma_mean=bear.vec(mean(sigma,3));
+        % step 5: draw the series of sigmas
+        % loop over units
+        for ii=1:numCountries
 
-      lambda_posterior=lambda1;
+            % compute Stilde
+            Stilde=(Yi(:,:,ii)-Xi(:,:,ii)*reshape(beta(:,ii),k,numEndog))'*(Yi(:,:,ii)-Xi(:,:,ii)*reshape(beta(:,ii),k,numEndog));
 
-      % store sample results
-      smpl = struct();
-      smpl.beta = beta_gibbs;
-      smpl.sigma = sigma_gibbs;
+            % draw sigma
+            sigma(:,:,ii)=bear.iwdraw(Stilde,T);
+
+            sigma_gibbs(:,ii)=bear.vec(sigma(:,:,ii));
+
+        end
+
+        beta_mean=b;
+
+        sigma_mean=bear.vec(mean(sigma,3));
+
+        lambda_posterior=lambda1;
+
+        % store sample results
+        smpl = struct();
+        smpl.beta = beta_gibbs;
+        smpl.sigma = sigma_gibbs;
       
     end
 
     % Burning part before returning the sampler
-    count = 1;
-    while count<Bu
-      sampler();
-      count = count+1;
+    for count=1:Bu
+        sampler();
     end
 
     outSampler = @sampler;
