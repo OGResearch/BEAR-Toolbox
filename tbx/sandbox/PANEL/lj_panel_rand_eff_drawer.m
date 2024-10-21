8926c058ad4f2c4927ede1eb05340687da8866ed
Ljubica Trajcev <ljubica.trajcev@ogresearch.com>
2024-10-21 09:01:14 +0200

    panel random effect sampler and drawer

--- tbx/sandbox/PANEL/lj_panel_rand_eff_drawer.m
+++ tbx/sandbox/PANEL/lj_panel_rand_eff_drawer.m
@@ -1,22 +1,28 @@
-function [drawerH, timelessDrawerH] = lj_panel_rand_eff_drawer(meta)
-    % input 
-    % smpl - one sample (gibbs sampling) that contains:
-    % smpl.beta - one sample of beta gibbs
-    % smpl.sigma - one sample of sigma gibbs
-
-    % output
-    % A - transformed matrix of parameters in front of transition variables
-    % C - tranformed matrix of parameters in front of exogenous and constant
-    % Sigma - transformed matrix of variance covariance of shocks
-    % Y = (L)Y*A + X*C + eps
+function [outUnconditionalDrawer, outIdentifierDrawer] = lj_panel_rand_eff_drawer(this, meta)
 
     numCountries = meta.numCountries;
     numEndog     = meta.numEndog;
     numLags      = meta.numLags;
     numExog      = meta.numExog;
+    
+    %IRF periods
+    IRFperiods = meta.IRFperiods;
 
-    function drawer = drawer(sampleStruct, horizon)
+    EstimationSpan = this.EstimationSpan;
 
+    function [As, Cs, Sigma] = identificationDrawer(sampleStruct)
+
+        % input 
+        % smpl - one sample (gibbs sampling) that contains:
+        % smpl.beta - one sample of beta gibbs
+        % smpl.sigma - one sample of sigma gibbs
+
+        % output
+        % A - transformed matrix of parameters in front of transition variables
+        % C - tranformed matrix of parameters in front of exogenous and constant
+        % Sigma - transformed matrix of variance covariance of shocks
+        % Y = (L)Y*A + X*C + eps
+          
         smpl = sampleStruct;
         beta = smpl.beta;
         sigma = smpl.sigma;
@@ -27,60 +33,68 @@ function [drawerH, timelessDrawerH] = lj_panel_rand_eff_drawer(meta)
 
         Sigma = [];
 
+        % initialize the output
+        As = cell(IRFperiods,1);
+        Cs = cell(IRFperiods,1);
+
         % iterate over countries
         for ii = 1:numCountries
 
-          beta_temp = reshape(...
-                  beta(:,ii),...
-                  numEndog*numLags+numExog,...
-                  numEndog...
-                  );
+            beta_temp = reshape(...
+                    beta(:,ii),...
+                    numEndog*numLags+numExog,...
+                    numEndog...
+                    );
 
-          sigma_temp = reshape(...
-                  sigma(:,ii),...
-                  numEndog,...
-                  numEndog...
-                  );
-                  
-          % Pack in blocks
-          a_temp = beta_temp(1:numEndog*numLags,:);
+            sigma_temp = reshape(...
+                    sigma(:,ii),...
+                    numEndog,...
+                    numEndog...
+                    );
+                    
+            % Pack in blocks
+            a_temp = beta_temp(1:numEndog*numLags,:);
 
-          c_temp = beta_temp(numEndog*numLags+1:end,:);
+            c_temp = beta_temp(numEndog*numLags+1:end,:);
 
-          A = blkdiag(A, a_temp);
+            A = blkdiag(A, a_temp);
 
-          C = [C, c_temp];
+            C = [C, c_temp];
 
-          Sigma = blkdiag(Sigma,sigma_temp);
+            Sigma = blkdiag(Sigma,sigma_temp);
 
         end
 
-        % initialize the output
-        drawer = struct();
-        drawer.As = cell(horizon,1);
-        drawer.Cs = cell(horizon,1);
-        drawer.Sigmas = cell(horizon,1);
-
         % pack the output
-        for tt = 1:horizon
+        for tt = 1:IRFperiods
 
-          drawer.As{tt} = A;
-          drawer.Cs{tt} = C;
-          drawer.Sigmas{tt} = Sigma;
+            As{tt} = A;
+            Cs{tt} = C;
 
         end
 
     end
 
-    
-    function timelessDrawer = timelessDrawer(sampleStruct, start, horizon)
 
-      timelessDrawer = drawer(sampleStruct, horizon);
+    function [As, Cs, Sigmas] = unconditionalDrawer(sampleStruct, forecastStart,forecastHorizon)
+
+        % call the identificationDrawer as for the time invariant models, results are almost the same
+        [As, Cs, Sigma] = identificationDrawer(sampleStruct, forecastHorizon);
+
+        Sigmas  = cell(forecastHorizon,1);
+
+        % pack the output
+        for tt = 1:forecastHorizon
+
+            Sigmas{tt} = Sigma;
+
+        end
 
     end
 
     % return function calls
-    drawerH = @drawer;
-    timelessDrawerH = @timelessDrawer;
+    outIdentifierDrawer = @identificationDrawer;
+
+    outUnconditionalDrawer = @unconditionalDrawer;
 
 end
--- tbx/sandbox/PANEL/lj_panel_rand_eff_smpl.m
+++ tbx/sandbox/PANEL/lj_panel_rand_eff_smpl.m
@@ -1,4 +1,4 @@
-function outSampler = lj_panel_rand_eff_smpl(meta, hyper, longY, longX, longZ)
+function outSampler = lj_panel_rand_eff_smpl(this, meta, longY, longX, longZ)
 
     % input
     % meta - model specific meta parameters
@@ -13,13 +13,13 @@ function outSampler = lj_panel_rand_eff_smpl(meta, hyper, longY, longX, longZ)
     const = meta.flagConst;
     lags  = meta.numLags;
 
-    lambda1 = hyper.lambda1;
+    lambda1 = this.Settings.lambda1;
 
     % compute preliminary elements
-    [Xi, Xibar, Xbar, Yi, yi, y, N, n, m, p, T, k, q, h]=bear.panel3prelim(longY,longX,const,lags);
+    [~, Xibar, Xbar, ~, yi, y, N, n, ~, ~, ~, ~, q, h]=bear.panel3prelim(longY,longX,const,lags);
 
     % obtain prior elements
-    [b, bbar, sigeps]=bear.panel3prior(Xibar,Xbar,yi,y,N,q);
+    [~, bbar, sigeps]=bear.panel3prior(Xibar,Xbar,yi,y,N,q);
 
     % compute posterior distribution parameters
     [omegabarb, betabar]=bear.panel3post(h,Xbar,y,lambda1,bbar,sigeps);
@@ -27,27 +27,27 @@ function outSampler = lj_panel_rand_eff_smpl(meta, hyper, longY, longX, longZ)
     % sampler will be a function
     function smpl = sampler()
 
-      % draw a random vector beta from N(betabar,omegabarb)
-      % TODO - optimize chol (can be run only once)
-      beta=betabar+chol(bear.nspd(omegabarb),'lower')*mvnrnd(zeros(h,1),eye(h))';
+        % draw a random vector beta from N(betabar,omegabarb)
+        % TODO - optimize chol (can be run only once)
+        beta=betabar+chol(bear.nspd(omegabarb),'lower')*mvnrnd(zeros(h,1),eye(h))';
 
-      beta=reshape(beta,q,N);
-      % record values by marginalising over each unit
-      for jj=1:N
+        beta=reshape(beta,q,N);
+        % record values by marginalising over each unit
+        for jj=1:N
 
-        beta_gibbs(:,jj)=beta(:,jj);
+            beta_gibbs(:,jj)=beta(:,jj);
 
-      end
+        end
 
-      % obtain a record of draws for sigma, the residual variance-covariance matrix
-      % compute sigma
-      sigma=sigeps*eye(n);
+        % obtain a record of draws for sigma, the residual variance-covariance matrix
+        % compute sigma
+        sigma=sigeps*eye(n);
 
-      sigma_gibbs=repmat(sigma(:),[1 N]);
-      
-      smpl = struct();
-      smpl.beta = beta_gibbs;
-      smpl.sigma = sigma_gibbs;
+        sigma_gibbs=repmat(sigma(:),[1 N]);
+        
+        smpl = struct();
+        smpl.beta = beta_gibbs;
+        smpl.sigma = sigma_gibbs;
 
     end
 
