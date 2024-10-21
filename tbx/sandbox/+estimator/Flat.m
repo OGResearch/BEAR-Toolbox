4609a93bb30f713e8094e299ead5cf6708eff1e7
kobercs13 <csaba.kober@ogresearch.com>
2024-10-21 12:49:10 +0200

    adding Flat prior

--- tbx/sandbox/+estimator/Flat.m
+++ tbx/sandbox/+estimator/Flat.m
@@ -1,5 +1,5 @@
 
-classdef NormalDiffuse < estimator.Base
+classdef Flat < estimator.Base
 
     methods
         function initializeSampler(this, YXZ)
@@ -25,52 +25,14 @@ classdef NormalDiffuse < estimator.Base
             opt.It = options.Burnin + numPresample;
             opt.Bu = options.Burnin;
 
-            opt.priorsexogenous = this.Settings.Exogenous;
-            opt.user_ar = this.Settings.Autoregression;
-            opt.lambda1 = this.Settings.Lambda1;
-            opt.lambda2 = this.Settings.Lambda2;
-            opt.lambda3 = this.Settings.Lambda3;
-            opt.lambda4 = this.Settings.Lambda4;
-            opt.lambda5 = this.Settings.Lambda5;
-
-        %     if isscalar(opt.lambda4)
-        %         opt.lambda4 = repmat(opt.lambda4, numEn, numEx);
-        %     end
-
-            sigmaAdapter = struct();
-            sigmaAdapter.none = 41;
-            opt.prior = sigmaAdapter.(lower(this.Settings.Sigma));
-
             opt.const = this.Settings.HasConstant;
             opt.p = this.Settings.Order;
 
             opt.bex  = this.Settings.BlockExogenous;
 
-            [Bhat, ~, ~, LX, ~, Y, ~, ~, ~, numEn, numEx, ~, estimLength, numBRows, sizeB] = ...
+            [Bhat, ~, ~, LX, ~, Y, ~, ~, ~, numEn, ~, ~, estimLength, ~, sizeB] = ...
                 bear.olsvar(Y_long, X_long, opt.const, opt.p);
 
-            priorexo = this.Settings.Exogenous;
-
-            % individual priors 0 for default
-        %     if isscalar(priorexo)
-        %         priorexo = repmat(priorexo, numEn, numEx);
-        %     end
-
-            %create a vector for AR hyperparamters
-        %     if isscalar(this.Settings.Autoregression)
-        %         this.Settings.Autoregression = repmat(this.Settings.Autoregression, numEn, 1);
-        %     end
-            ar = this.Settings.Autoregression;
-
-            %variance from univariate OLS for priors
-            % arvar = bear.arloop(Y_long, opt.const, opt.p, numEn);
-            arvar = bear.arloop(Y_long, opt.const, opt.p, numEn);
-
-            %setting up prior
-            [beta0, omega0] = bear.ndprior(ar, arvar, opt.lambda1, opt.lambda2, opt.lambda3, opt.lambda4, opt.lambda5, ...
-                numEn, numEx, opt.p, numBRows, sizeB, opt.bex, blockexo, priorexo);
-
-            invomega0 = diag(1./diag(omega0));
             B = Bhat;
 
             %===============================================================================
@@ -95,19 +57,16 @@ classdef NormalDiffuse < estimator.Base
                 invsigma = invC * invC';
 
                 % then obtain the omegabar matrix
-                invomegabar = invomega0 + kron(invsigma, LX' * LX);
+                invomegabar = kron(invsigma, LX' * LX);
                 C = chol(bear.nspd(invomegabar));
                 invC = C \ speye(sizeB);
                 omegabar = invC * invC';
 
                 % following, obtain betabar
-                betabar = omegabar * (invomega0 * beta0 + kron(invsigma, LX') * Y(:));
+                betabar = omegabar * (kron(invsigma, LX') * Y(:));
 
                 % draw from N(betabar,omegabar);
                 beta = betabar + chol(bear.nspd(omegabar),'lower') * mvnrnd(zeros(sizeB,1),eye(sizeB))';
-
-                % update matrix B with each draw
-                B = reshape(beta,size(B));
 
                 sampleStruct.beta = beta;
                 sampleStruct.sigma = sigma;
