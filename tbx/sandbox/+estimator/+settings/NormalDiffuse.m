e05f43e2b94634b1be7cde651ea662be8d4a732e
kobercs13 <csaba.kober@ogresearch.com>
2024-10-22 17:45:23 +0200

    delete obsolete parts

--- tbx/sandbox/+estimator/+settings/NormalDiffuse.m
+++ tbx/sandbox/+estimator/+settings/NormalDiffuse.m
@@ -1,9 +1,5 @@
 
 classdef (CaseInsensitiveProperties=true) NormalDiffuse < estimator.settings.Base
 
-    properties     
-        Sigma (1, 1) string {ismember(Sigma, ["none"])} = "none"
-    end
-
 end
 
