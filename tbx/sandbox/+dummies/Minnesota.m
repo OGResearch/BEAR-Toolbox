7a0ee56958e00a15cb33692ef4f5283196e23233
kobercs13 <csaba.kober@ogresearch.com>
2024-10-22 14:27:33 +0200

    Update Minnesota.m
    make minnesota working

--- tbx/sandbox/+dummies/Minnesota.m
+++ tbx/sandbox/+dummies/Minnesota.m
@@ -4,8 +4,9 @@ classdef Minnesota < settings.Base
     properties
         Lambda (1, 1) double = 0.1 % lambda1
         LagDecay (1, 1) double = 1 % lambda3
-        ExogenousLambda (1, 1) double = 100 % lambda4
-        Autoregression (1, 1) double = 0.8 % ar
+        ExogenousLambda (:, :) double = 100 % lambda4
+        Autoregression (:, :) double = 0.8 % ar
+        Exogenous (:, :) logical = false
     end
 
     methods
@@ -14,30 +15,56 @@ classdef Minnesota < settings.Base
             % [numY, numL, numX, ~, order] = system.getDimensionsFromYLX(initYLX);
             numY = meta.NumEndogenousNames;
             numX = meta.NumExogenousNames;
+            numXI = numX + double(meta.HasIntercept);
+
+            if isscalar(this.Exogenous)
+                this.Exogenous = repmat(this.Exogenous, numY, numXI);
+            end
+            if isscalar(this.ExogenousLambda)
+                this.ExogenousLambda = repmat(this.ExogenousLambda, numY, numXI);
+            end
+            if isscalar(this.Autoregression)
+                this.Autoregression = repmat(this.Autoregression, numY, 1);
+            end
+
+
             order = meta.Order;
-            %
-            [initY, initX] = initYXZ{:};
-            %
-            % dummiesY = [
-            %     diag(ar(1:numY, 1).*arvar/lambda1)
-            %     zeros(numY*(order-1), numY)
-            %     (priorexo./(lambda1.*lambda4))'
-            %     diag(arvar)
-            % ];
+            const = meta.HasIntercept;
 
-            % Jp = diag((1 : order) .^ this.LagDecay);
+            lambda1 = this.Lambda;
+            lambda3 = this.LagDecay;
+            lambda4 = this.ExogenousLambda; 
+            ar = this.Autoregression;
+            priorexo = this.Exogenous;
 
-            % Xm = [
-            %     kron(Jp, diag(arvar/lambda1)), zeros(numY*order, numX)
-            %     zeros(numX, numY*order), diag(1./(lambda1*lambda4(1, :)))
-            %     zeros(numY, numY*order), zeros(numY, numX)
-            % ]; % error if numX is equal to zero
+            [initY, ~, ~] = initYXZ{:};
 
-            dummiesY = zeros(0, numY);
-            dummiesL = zeros(0, numY*order);
-            dummiesX = zeros(0, numX);
+            %variance from univariate OLS for priors
+            arvar = bear.arloop(initY, const, order, numY);
 
-            dummiesYLX = {dummiesY, dummiesL, dummiesX};
+            dummiesY = [
+                diag(ar(1:numY, 1) .* arvar / lambda1)
+                zeros(numY * (order-1), numY)
+                (priorexo ./ (lambda1 .* lambda4))'
+                diag(arvar)
+            ];
+
+            Jp = diag((1 : order) .^ lambda3);
+
+            if numXI ~= 0
+                dummiesLX = [
+                    kron(Jp, diag(arvar/lambda1)), zeros(numY*order, numXI)
+                    zeros(numXI, numY*order), diag(1./(lambda1*lambda4(1, :)))
+                    zeros(numY, numY*order), zeros(numY, numXI)
+                    ];
+            else
+                dummiesLX = [
+                    kron(Jp, diag(arvar/lambda1))
+                    zeros(numY, numY*order)
+                    ];
+            end
+            
+            dummiesYLX = {dummiesY, dummiesLX};
         end%
     end
 
