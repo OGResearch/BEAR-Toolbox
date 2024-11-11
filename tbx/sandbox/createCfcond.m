fb35d5bcf3417a89ed860550f13e8111a6bb87de
kobercs13 <csaba.kober@ogresearch.com>
2024-11-11 17:55:34 +0100

    create cf components

---
+++ tbx/sandbox/createCfcond.m
@@ -0,0 +1,24 @@
+
+function cfconds = createCfcond(meta, longY, condTable, forecastSpan)
+
+    order = meta.Order;
+
+    shortY = longY(order + 1:end, :);
+    endoNames = meta.EndogenousNames;
+    numEn = numel(meta.EndogenousNames);
+
+    fcastLength = numel(fcastSpan);
+    cfconds = cell(fcastLength, numEn);
+    
+    for nn = 1 : numEn
+        tmp = condTable(:, endoNames(nn));
+        nonEmptyRows = ~cellfun(@isempty, tmp.Variables);
+        nonEmptyTimes = tmp.Time(nonEmptyRows);
+        [~, ix] = ismember(nonEmptyTimes, forecastSpan);
+        ix = ix(ix>0);
+        if ~isempty(ix)
+            cfconds{ix, nn} = shortY(ix, nn);
+        end
+    end
+
+end
