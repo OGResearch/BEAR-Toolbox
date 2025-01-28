dbc18a640cfb9ddafd91c02a7cc5980af2fbc7f5
Ljubica Trajcev <ljubica.trajcev@ogresearch.com>
2025-01-28 09:00:05 +0100

    add drawers

--- tbx/sandbox/+estimator/MixedFrequencyBVAR.m
+++ tbx/sandbox/+estimator/MixedFrequencyBVAR.m
@@ -588,11 +588,61 @@ classdef MixedFrequencyBVAR < estimator.Base
         end
 
         function createDrawers(this, meta)
-            this.IdentificationDrawer = [];
-            this.HistoryDrawer = [];
-            this.UnconditionalDrawer = [];
-            this.ConditionalDrawer = [];
-        end
+            %[
+            numEndog = meta.NumEndogenousConcepts;
+            numRowsA = numEndog*meta.Order;
+            numExog = meta.NumExogenousNames+double(meta.HasIntercept);
+            numRowsB = numRowsA + numExog;
+            estimationHorizon = numel(meta.ShortSpan);
+            identificationHorizon = meta.IdentificationHorizon;
+            wrap = @(x, horizon) repmat({x}, horizon, 1);
+
+            function [A, C] = betaDrawer(sample, horizon)
+                beta = reshape(sample.beta, numRowsB, numEndog);
+                A = beta(1:numRowsA,:);
+                C = beta(numRowsA+1:end,:);
+                if horizon > 0
+                    A = wrap(A, horizon);
+                    C = wrap(C, horizon);
+                end
+            end%
+
+            function sigma = sigmaDrawer(sample, horizon)
+                sigma = reshape(sample.sigma, numEndog, numEndog);
+                if horizon > 0
+                    sigma = wrap(sigma, horizon);
+                end
+            end%
+
+            function draw = identificationDrawer(sample)
+                draw = struct();
+                [draw.A, draw.C] = betaDrawer(sample, identificationHorizon);
+                draw.Sigma = sigmaDrawer(sample, 0);
+            end%
+
+            function draw = unconditionalDrawer(sample, startIndex, forecastHorizon)
+                draw = struct();
+                [draw.A, draw.C] = betaDrawer(sample, forecastHorizon);
+                draw.Sigma = sigmaDrawer(sample, forecastHorizon);
+            end%
+
+            function draw = historyDrawer(sample)
+                draw = struct();
+                [draw.A, draw.C] = betaDrawer(sample, estimationHorizon);
+                draw.Sigma = sigmaDrawer(sample, estimationHorizon);
+            end%
+
+            function draw = conditionalDrawer(sample, startIndex, forecastHorizon)
+                draw = struct();
+                draw.beta = wrap(sample.beta, forecastHorizon);
+            end%
+
+            this.IdentificationDrawer = @identificationDrawer;
+            this.HistoryDrawer = @historyDrawer;
+            this.UnconditionalDrawer = @unconditionalDrawer;
+            this.ConditionalDrawer = @conditionalDrawer;
+            %]
+        end%
 
     end
 end
