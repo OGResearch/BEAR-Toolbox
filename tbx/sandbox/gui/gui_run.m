dbb74529b3287ec2ccc2302d9435e87c981342e3
Jaromír Beneš <jaromir.benes@gmail.com>
2025-05-30 11:26:45 +0200

    Checkpoint

--- tbx/sandbox/gui/gui_run.m
+++ tbx/sandbox/gui/gui_run.m
@@ -3,7 +3,25 @@ function gui_run()
 
     disp("Loading input data");
 
+    inputDataFileSelection = gui.querySelection("InputDataFile");
+    disp("Input data file: " + inputDataFileSelection);
+
     disp("Estimating the model");
+
+    estimatorSelection = gui.querySelection("Estimator");
+    disp("Estimator: " + estimatorSelection);
+
+    estimatorSettingsPath = fullfile(".", "settings", "estimatorSettings.json");
+    estimatorSettings = jsondecode(fileread(estimatorSettingsPath));
+    settings = struct();
+    if estimatorSelection ~= ""
+        settings = estimatorSettings.(estimatorSelection);
+    end
+    disp("Estimator settings");
+    disp(settings);
+
+    identificationSelection = gui.querySelection("Identification");
+    disp("Identification: " + identificationSelection);
 
     disp("Running the tasks");
 
